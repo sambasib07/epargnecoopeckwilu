f7ba9cb8d8600a5d6effadf849faa9f003e92efe
sambasib07 <sambasib07@gmail.com>
2025-09-06 21:10:56 +0100

    sw.js

--- sw.js
+++ sw.js
@@ -1,45 +1,47 @@
-const CACHE_NAME = 'coopec-kwilu-v1';
+const CACHE_NAME = 'coopec-kwilu-v1.0';
 const urlsToCache = [
-    '/',
-    '/index.html',
-    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
-    'https://i.postimg.cc/RNH2v9sC/logo-coopec-Kwilu.jpg',
-    '/manifest.json'
+  '/',
+  '/index.html',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+  'https://i.postimg.cc/RNH2v9sC/logo-coopec-Kwilu.jpg'
 ];
 
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then((cache) => {
-                console.log('Opened cache');
-                return cache.addAll(urlsToCache);
-            })
-    );
+// Installation du Service Worker
+self.addEventListener('install', event => {
+  console.log('Service Worker installation en cours');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache ouvert');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request)
-            .then((response) => {
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request);
-            })
-    );
-});
-
-self.addEventListener('activate', (event) => {
-    const cacheWhitelist = [CACHE_NAME];
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((cacheName) => {
-                    if (cacheWhitelist.indexOf(cacheName) === -1) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
+// Activation du Service Worker
+self.addEventListener('activate', event => {
+  console.log('Service Worker activation en cours');
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Suppression de l\'ancien cache:', cacheName);
+            return caches.delete(cacheName);
+          }
         })
-    );
+      );
+    })
+  );
+});
+
+// Interception des requêtes
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Retourne la réponse en cache ou fetch la requête
+        return response || fetch(event.request);
+      })
+  );
 });
